cb6e20b564d0a9fbbae682d4e77866769478326a
Anikeychik Dmitry <Dima716@yandex.ru>
2015-08-01 15:00:12 +0300

    Added stylus variables and made some refactoring of js code

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -1,17 +1,22 @@
 var fixedHeader = (function () {
 	'use strict';
 
-	var table = $('table'),
-			tableFixed = table.clone(),
-			oldScrollTop = $(window).scrollTop(),
-			newScrollTop,
-			api = {};
+	var oldScrollTop = $(window).scrollTop(),
+			newScrollTop;
+
+	// cache DOM
+	var $table = $('table'),
+			$tableFixed = $table.clone();
+
+	// bind events
+	$(window).on('resize', onResizeListener);
+	$(window).on('scroll', onScrollListener);
 
 	function onResizeListener() {
-		tableFixed.css({left: table.offset().left});
+		$tableFixed.css({left: $table.offset().left});
 
-		tableFixed.find('th').each(function (index) {
-				var newWidth = table
+		$tableFixed.find('th').each(function (index) {
+				var newWidth = $table
 												.find('th')
 												.eq(index)
 												.outerWidth();
@@ -29,92 +34,95 @@ var fixedHeader = (function () {
 		// Check if the scroll horizontal or vertical
 		if (oldScrollTop === newScrollTop) {
 			// horizontall scrolling
-			tableFixed.css({left: table.offset().left - $(window).scrollLeft()});
+			$tableFixed.css({left: $table.offset().left - $(window).scrollLeft()});
 		} else {
 				// vertical scrolling
 				oldScrollTop = newScrollTop;
 
 				var offset = newScrollTop,
-						tableOffsetTop = table.offset().top,
-						tableOffsetBottom = tableOffsetTop + table.height() - table.find('thead').height();
+						tableOffsetTop = $table.offset().top,
+						tableOffsetBottom = tableOffsetTop + $table.height() - $table.find('thead').height();
 
 				if (offset < tableOffsetTop || offset > tableOffsetBottom) {
-					tableFixed.hide();
-				} else if (offset >= tableOffsetTop && offset <= tableOffsetBottom && tableFixed.is(':hidden')) {
-					tableFixed.show();
+					$tableFixed.hide();
+				} else if (offset >= tableOffsetTop && offset <= tableOffsetBottom && $tableFixed.is(':hidden')) {
+					$tableFixed.show();
 				}
 		}
 	}
 
 	function init() {
-		tableFixed.find('tbody')
+		$tableFixed.find('tbody')
 								.remove()
 								.end()
 								.addClass('timetable_fixed')
-								.css({left: table.offset().left})
-								.insertBefore(table);
-
-		$(window).on('resize', onResizeListener);
-		$(window).on('scroll', onScrollListener);
+								.css({left: $table.offset().left})
+								.insertBefore($table);
 		onResizeListener();
 	}
 
-	api.init = init;
+	var api = {
+		init: init
+	};
 
 	return api;
 })(),
 filteredRows = (function () {
 	'use strict';
-	var tbody = $('tbody'),
-			allRows = tbody.find('tr'),
-			landingRows = allRows.filter('.landing, .landing + .flight-details'),
-			takeoffRows = allRows.filter('.takeoff, .takeoff + .flight-details'),
-			message = $('.message'),
-			takeoffCheck = $('#takeoff-check'),
-			landingCheck = $('#landing-check'),
-			api = {};
+
+	// cache DOM
+	var $tbody = $('tbody'),
+		$allRows = $tbody.find('tr'),
+		$landingRows = $allRows.filter('.landing, .landing + .flight-details'),
+		$takeoffRows = $allRows.filter('.takeoff, .takeoff + .flight-details'),
+		$message = $('.message'),
+		$takeoffCheck = $('#takeoff-check'),
+		$landingCheck = $('#landing-check');
+
+	// bind events
+	$('#takeoff-check').on('change', onTakeoffChange);
+	$('#landing-check').on('change', onLandingChange);
 
 	function onTakeoffChange() {
-		if (takeoffCheck.is(':checked')) {
-			message.hide();
-			$('tbody > tr').length ? tbody.append(allRows) : tbody.append(takeoffRows);
+		if ($takeoffCheck.is(':checked')) {
+			$message.hide();
+			$('tbody > tr').length ? $tbody.append($allRows) : $tbody.append($takeoffRows);
 		} else {
-			takeoffRows.remove();
+			$takeoffRows.remove();
 
-			if (landingCheck.is(':not(:checked)')) {
-				message.show();
+			if ($landingCheck.is(':not(:checked)')) {
+				$message.show();
 			}
 		}
 	}
 
 	function onLandingChange() {
-		if (landingCheck.is(':checked')) {
-			message.hide();
-			$('tbody > tr').length ? tbody.append(allRows) : tbody.append(landingRows);
+		if ($landingCheck.is(':checked')) {
+			$message.hide();
+			$('tbody > tr').length ? $tbody.append($allRows) : $tbody.append($landingRows);
 		} else {
-			landingRows.remove();
+			$landingRows.remove();
 
-			if (takeoffCheck.is(':not(:checked)')) {
-				message.show();
+			if ($takeoffCheck.is(':not(:checked)')) {
+				$message.show();
 			}
 		}
 	}
 
 	function init() {
-		if (takeoffCheck.is(':not(:checked)') && landingCheck.is(':not(:checked)')) {
-			allRows.remove();
-			message.show();
-		} else if (takeoffCheck.is(':not(:checked)')) {
-				landingRows.remove();
-		} else if (landingCheck.is(':not(:checked)')) {
-				takeoffRows.remove();
+		if ($takeoffCheck.is(':not(:checked)') && $landingCheck.is(':not(:checked)')) {
+			$allRows.remove();
+			$message.show();
+		} else if ($takeoffCheck.is(':not(:checked)')) {
+				$takeoffRows.remove();
+		} else if ($landingCheck.is(':not(:checked)')) {
+				$landingRows.remove();
 		}
-
-		$('#takeoff-check').on('change', onTakeoffChange);
-		$('#landing-check').on('change', onLandingChange);
 	}
 
-	api.init = init;
+	var api = {
+		init: init
+	};
 
 	return api;
 })();
